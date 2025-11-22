f59e4321dabc5ee6c5b766bf4ef6137c9b21bd4e
asadovk2 <kamalaa@code.edu.az>
2025-11-22 12:17:49 +0000

    f

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+$ amonkeyprogrammer@ubuntu:~/0x06$ cat 0-main.py
+#!/usr/bin/python3
+Square = __import__('0-square').Square
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
+
+$ amonkeyprogrammer@ubuntu:~/0x06$ ./0-main.py
+<class '0-square.Square'>
+{}
+$ amonkeyprogrammer@ubuntu:~/0x06$
